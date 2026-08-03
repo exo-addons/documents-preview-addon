c0e67a7c292b0460261bf83634bf2fb97a7854d5
Houssem eXo <hbenali@exoplatform.fr>
2026-08-03 17:43:17 +0200

    fix: align test property names with renamed preview config keys

--- services/src/test/java/org/exoplatform/addons/documentspreview/DocumentsPreviewRestTest.java
+++ services/src/test/java/org/exoplatform/addons/documentspreview/DocumentsPreviewRestTest.java
@@ -51,9 +51,9 @@ import jakarta.servlet.http.HttpServletRequest;
  */
 public class DocumentsPreviewRestTest {
 
-  private static final String MAX_FILE_SIZE_PROPERTY_NAME = "exo.ecms.documents.pdfviewer.max-file-size";
+  private static final String MAX_FILE_SIZE_PROPERTY_NAME = "exo.documents.preview.max-file-size";
 
-  private static final String MAX_PAGES_PROPERTY_NAME     = "exo.ecms.documents.pdfviewer.max-pages";
+  private static final String MAX_PAGES_PROPERTY_NAME     = "exo.documents.preview.max-pages";
 
   private static final String DOCUMENT_ID                 = "42";
 
